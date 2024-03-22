f17d6eacd8767dd8d0183a29809f766866951e65
Khoa Le <khoatle165@gmail.com>
2024-03-22 01:29:37 -0400

    Initial data

---
+++ index.js
@@ -0,0 +1,372 @@
+export const mentorshipTeam = [
+	{
+		name: 'Khoa Le',
+		title: 'Software Engineer @ Vanta',
+		hobbies: 'boardgames, chess, reality shows',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/q_100/v1577895922/portfolio/avatar.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Program Lead / Mentor',
+			},
+		],
+	},
+	{
+		name: 'Huy Anh',
+		title: 'Software Engineer @ Netflix',
+		hobbies: 'cats, startups, capybara',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083192/viettech/huyanh.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Lead / Mentor',
+			},
+		],
+	},
+	{
+		name: 'Chan Le',
+		title: 'Viet Tech Founder',
+		hobbies: 'real estate, real estate, real estate',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083254/viettech/chanle.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Program Advisor',
+			},
+		],
+	},
+	{
+		name: 'Nguyen Vu',
+		title: 'Software Engineer @ Mastercard',
+		hobbies: 'hiking, nature, cooking',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083321/viettech/nguyen.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Lead / Mentor',
+			},
+		],
+	},
+	{
+		name: 'Thu Anh',
+		title: 'Software Engineer @ Mastercard',
+		hobbies: 'reading, writing, corgi',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083321/viettech/thuanh.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Lead / Mentor',
+			},
+		],
+	},
+	{
+		name: 'Kevin Luu',
+		title: 'Software Engineer @ Anyscale',
+		hobbies: 'travel, pickle ball, badminton',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083321/viettech/kevin.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Lead / Mentor',
+			},
+		],
+	},
+	{
+		name: 'Jenny Quach',
+		title: '[Incoming] SWE Intern @ Wayfair',
+		hobbies: 'board games, anime, food',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/jenny.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Lead / Mentor',
+			},
+		],
+	},
+	{
+		name: 'Son Mac',
+		title: 'Software Engineer @ Vanta',
+		hobbies: 'frontend, cats, travel',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083321/viettech/son.jpg',
+		term: [
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Nick Doan',
+		title: '[Incoming] Software Engineer @ Palantir',
+		hobbies: 'coding, running, games',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083513/viettech/nick.jpg',
+		term: [
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Robin Hoang',
+		title: '[Incoming] SWE Intern @ Microsoft',
+		hobbies: 'solo trip, pole dance, beach',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/robin.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Khue Tang',
+		title: '[Incoming] SWE Intern @ LinkedIn',
+		hobbies: 'pekking duck, cooking, hiking',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/khue.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Ngoc Doan',
+		title: '[Incoming] SWE Intern @ DoorDash',
+		hobbies: 'road trip, food, concert',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/placeholder.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Dang Pham',
+		title: '[Incoming] SWE Intern @ ServiceNow',
+		hobbies: 'reading, writing, gaming',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/dang.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Phuong Cao',
+		title: '[Incoming] SWE Intern @ Adobe',
+		hobbies: 'pekking duck, Duc Anh, music',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/minhphuong.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Phuoc Ong',
+		title: '[Incoming] SWE Intern @ ServiceNow',
+		hobbies: 'gym, food, dog',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/phuoc.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Hieu Vuong',
+		title: 'SWE Intern @ SAP',
+		hobbies: 'karaoke, beauty salon, travel',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/hieu.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Mentor',
+			},
+		],
+	},
+	{
+		name: 'Cuong Nguyen',
+		title: '[Incoming] SWE Intern @ GoDaddy',
+		hobbies: 'board games, PC games, anime',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/cuong.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Assistant',
+			},
+		],
+	},
+	{
+		name: 'Quan Le',
+		title: '[Incoming] SWE Intern @ Matson Money',
+		hobbies: 'cosplay, board games, music',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/minhquan.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentor',
+				role: 'Assistant',
+			},
+		],
+	},
+	{
+		name: 'Trang Vu',
+		title: 'Student @ York University',
+		hobbies: 'board games, K-drama, flowers',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711084156/viettech/trangvu.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentee',
+				role: 'Assistant',
+			},
+		],
+	},
+	{
+		name: 'Thanh Nguyen',
+		title: 'Student @ University of British Columbia',
+		hobbies: 'beach, solo travel, writing',
+		avatar:
+			'https://res.cloudinary.com/khoa165/image/upload/v1711083621/viettech/thanh.jpg',
+		term: [
+			{
+				year: 2023,
+				keyword: 'mentee',
+				role: 'Mentee',
+			},
+			{
+				year: 2024,
+				keyword: 'mentee',
+				role: 'Assistant',
+			},
+		],
+	},
+];
